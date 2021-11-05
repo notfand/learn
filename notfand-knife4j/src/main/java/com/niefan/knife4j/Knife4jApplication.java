1a175a6f4818deba4f4db75b573bf53d1b6a9e2e
聂凡 <14363@9ji.com>
2021-11-05 13:41:09 +0800

    add: 学习knife4j

---
+++ notfand-knife4j/src/main/java/com/niefan/knife4j/Knife4jApplication.java
@@ -0,0 +1,16 @@
+package com.niefan.knife4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Niefan
+ * @date 2021/11/5
+ */
+@SpringBootApplication
+public class Knife4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knife4jApplication.class, args);
+	}
+}
---
+++ notfand-knife4j/src/main/java/com/niefan/knife4j/config/Swagger2Config.java
@@ -0,0 +1,18 @@
+package com.niefan.knife4j.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2 文档配置
+ * @author Niefan
+ * @date 2021/11/5
+ */
+@Configuration
+@EnableKnife4j
+@EnableSwagger2
+public class Swagger2Config {
+
+
+}
